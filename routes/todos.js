b0b5e989475bf95522941712fee5c4a372706877
Ashutosh Singh <ashutosh.singh32@infosys.com>
2023-06-11 14:47:43 +0530

    created getTodo route and controller

--- routes/todos.js
+++ routes/todos.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router();
 const {createTodo} = require('../controllers/createTodo');
-
+const {getTodo} = require('../controllers/getTodo');
 //define API routes
 router.post('/createTodo', createTodo);
-
+router.get('/getTodos', getTodo);
 
 module.exports = router;
