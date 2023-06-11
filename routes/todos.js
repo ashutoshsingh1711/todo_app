ea0fa71b1b396fe4c863c93278484af05c076d4c
Ashutosh Singh <ashutosh.singh32@infosys.com>
2023-06-11 15:13:43 +0530

    Created updation of data route and controller

--- routes/todos.js
+++ routes/todos.js
@@ -2,10 +2,13 @@ const express = require('express')
 const router = express.Router();
 const {createTodo} = require('../controllers/createTodo');
 const {getTodo,getTodoById} = require('../controllers/getTodo');
+const {updateTodo} = require('../controllers/updateTodo');
+
 //define API routes
 router.post('/createTodo', createTodo);
 router.get('/getTodos', getTodo);
-router.get('/getTodo/:id', getTodoById)
+router.get('/getTodo/:id', getTodoById);
+router.put('/updateTodo/:id', updateTodo)
 
 
 module.exports = router;
